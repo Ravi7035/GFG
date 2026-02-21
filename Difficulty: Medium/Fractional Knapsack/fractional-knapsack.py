65b10669974e88f9b504227812b97ca427a83edf
Ravi7035 <ravimuliki7035@gmail.com>
2026-02-21 19:36:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Fractional Knapsack/fractional-knapsack.py
@@ -0,0 +1,33 @@
+class Solution:
+    def fractionalKnapsack(self, val, wt, capacity):
+        #code here
+        items=[]
+        for i in range(len(wt)):
+            
+            items.append([val[i]/wt[i],val[i],wt[i]])
+        
+        items.sort(reverse=True)
+            
+        total_value=0.0
+        
+        for [ratio,value,weight] in items:
+            
+            if capacity >= weight:
+                
+                total_value+=value
+                
+                capacity-=weight
+                
+            else:
+                total_value+=ratio*capacity
+                break
+            
+        return total_value
+                
+        
+        
+        
+                
+                
+                
+        

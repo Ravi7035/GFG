4f333cee075c12e131f5033831f68ab489f3fde3
Ravi7035 <ravimuliki7035@gmail.com>
2026-02-25 14:44:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Josephus problem/josephus-problem.py
+++ Difficulty: Easy/Josephus problem/josephus-problem.py
@@ -1,8 +1,19 @@
 class Solution:
     def josephus(self, n, k):
+        """
         ans=0
         for i in range(2,n+1):
             ans=(ans+k)%i
         return ans+1
-        
+        """
+        def josephus(n,k):
+            
+            if n==1:
+                return 0
+               
+            return (josephus(n-1,k)+k)%n
+            
+        return josephus(n,k)+1
+
+            
        

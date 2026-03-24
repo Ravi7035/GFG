214e71b23494b960ad89910d36b43c0ad8783f48
MULIKI VEERA NAGA BHADRA RAVI TEJA <ravimuliki7035@gmail.com>
2026-03-24 12:47:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.py
@@ -0,0 +1,24 @@
+class Solution:
+    def longCommSubstr(self, s1, s2):
+        m=len(s1)
+        n=len(s2)
+    
+        dp=[[0]*(n+1) for _ in range(m+1)]
+        
+        max_len=0
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                
+                if s1[i-1]==s2[j-1]:
+                    dp[i][j]=1+dp[i-1][j-1]
+                    max_len=max(max_len,dp[i][j])
+                else:
+                    dp[i][j]=0
+                    
+        return max_len
+                
+            
+           
+                
+                
+        

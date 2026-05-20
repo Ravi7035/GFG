fc13defbbd5506826a66809bab5ab85977fc7567
MULIKI VEERA NAGA BHADRA RAVI TEJA <ravimuliki7035@gmail.com>
2026-05-20 10:13:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Product Pair/product-pair.py
@@ -0,0 +1,30 @@
+class Solution:
+
+    def isProduct(self, arr, target):
+
+        seen=set()
+       
+        for num in arr:
+           
+            if num ==0:
+                
+               if target==0:
+                   
+                   return True
+                   
+            else:
+                
+                if target%num==0:
+                    
+                    needed =target//num
+                    
+                    if needed in seen:
+                        
+                        return True
+                        
+            seen.add(num)
+            
+        return False
+                
+                
+        

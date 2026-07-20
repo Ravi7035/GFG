a7831ca8821d926ee151d24783d3f82f081aadc5
MULIKI VEERA NAGA BHADRA RAVI TEJA <ravimuliki7035@gmail.com>
2026-07-20 15:31:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Shortest Unique Prefix for Every Word/shortest-unique-prefix-for-every-word.py
@@ -0,0 +1,35 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.count = 0
+
+
+class Solution:
+    def findPrefixes(self, arr):
+        root = TrieNode()
+
+        # Insert words into Trie
+        for word in arr:
+            node = root
+            for ch in word:
+                if ch not in node.children:
+                    node.children[ch] = TrieNode()
+                node = node.children[ch]
+                node.count += 1
+
+        ans = []
+
+        # Find shortest unique prefix
+        for word in arr:
+            node = root
+            prefix = ""
+
+            for ch in word:
+                node = node.children[ch]
+                prefix += ch
+                if node.count == 1:
+                    break
+
+            ans.append(prefix)
+
+        return ans

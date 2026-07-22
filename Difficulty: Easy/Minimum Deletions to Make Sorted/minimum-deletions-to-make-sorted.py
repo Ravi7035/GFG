fdb4ee201987f3189fae3bd7c4054c4699e771e9
MULIKI VEERA NAGA BHADRA RAVI TEJA <ravimuliki7035@gmail.com>
2026-07-22 23:46:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Minimum Deletions to Make Sorted/minimum-deletions-to-make-sorted.py
@@ -0,0 +1,16 @@
+from bisect import bisect_left
+
+class Solution:
+    def minDeletions(self, arr):
+        lis = []
+
+        for x in arr:
+            idx = bisect_left(lis, x)
+            if idx == len(lis):
+                lis.append(x)
+            else:
+                lis[idx] = x
+
+        return len(arr) - len(lis)
+            
+        
